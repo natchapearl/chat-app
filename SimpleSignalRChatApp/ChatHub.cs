3afe98168686530bcecaac33972f451eead88dec
Natcha Kaovorakarn <natchapearl@gmail.com>
2017-09-12 14:31:12 -0700

    First commit

---
+++ SimpleSignalRChatApp/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SimpleSignalRChatApp
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            //sendMessage method is being called to update all clients.
+            Clients.All.sendMessage(name, message);
+        }
+    }
+}
---
+++ SimpleSignalRChatApp/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SimpleSignalRChatApp.Startup))]
+
+namespace SimpleSignalRChatApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Mapping hub to the app builder
+            app.MapSignalR();
+        }
+    }
+}
